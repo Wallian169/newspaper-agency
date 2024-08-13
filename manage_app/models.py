40d43a15b98b5e35ff9ea420f0ce171344312eaf
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-13 16:14:16 +0300

    created models im manage_app.models
    Created model Redactor that inherits AbstractUser, Topic model with ManyToMany relations to Newspaper and finally Newspaper model. Added AUTH_USER_MODEL to settings

--- manage_app/models.py
+++ manage_app/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Redactor(AbstractUser):
+    years_of_experience = models.PositiveSmallIntegerField()
+
+
+class Topic(models.Model):
+    name = models.CharField(
+        max_length=255
+    )
+
+
+class Newspaper(models.Model):
+    title = models.CharField(
+        max_length=150
+    )
+    content = models.TextField()
+    published_date = models.DateField()
+    publishers = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="publishers"
+    )
+    topics = models.ManyToManyField(Topic, related_name="topics")
