c1bd411cf5bc1942957de15af76ea17d0169d26e
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-14 12:28:25 +0300

    Addfeat: added dynamic representation of data from db

--- manage_app/views.py
+++ manage_app/views.py
@@ -1,6 +1,20 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from manage_app.models import Redactor, Newspaper, Topic
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "manage_app/index.html")
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+    }
+    return render(
+        request,
+        "manage_app/index.html",
+        context=context,
+    )
