dd527d40e8ea325ff2f3354aa337a24a7b7cab64
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-16 11:41:37 +0300

    created 'templatetags' dir inside manage_app, created 'query_transform' custom tag
    This special tag solves the problem with pagination when going to another page erases the recorded search value.

--- manage_app/views.py
+++ manage_app/views.py
@@ -30,6 +30,7 @@ def index(request: HttpRequest) -> HttpResponse:
 
 class TopicListView(generic.ListView):
     model = Topic
+    paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
