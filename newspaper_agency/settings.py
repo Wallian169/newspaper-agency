47fc46fd9f8a624b009d5ff38b02e6de283e0fc6
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-20 09:11:21 +0300

    fixed link in 'topic_list.html'

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-a-7d^t#t)y+mmz=bv6q%a6=ps+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
